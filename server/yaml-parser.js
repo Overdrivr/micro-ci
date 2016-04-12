bed01db15fe15fb630d09dea32b1a51bd402e1aa
Dan <dan.faudemer@gmail.com>
2016-04-12 07:48:53 +0200

    Correct bug found by unit testing

--- server/yaml-parser.js
+++ server/yaml-parser.js
@@ -1,32 +1,35 @@
 var yaml = require("yaml");
 var inArray = require("in-array");
-function yaml_parser (stringYaml, parsed)
+function yaml_parser (stringYaml)
 {
   var parsed = [];
   parsed = yaml.eval(stringYaml);
   yaml_sanitycheck(parsed);
+  return parsed;
 }
 
 function yaml_sanitycheck(parsedYaml)
 {
-  var supportedOptions = ["build", "after_success" "after_failure", "deploy", "platforms", "binary_file"];
-  var neededOptions = ["build", "after_success" "after_failure", "platforms"]
+  //console.log(parsedYaml);
+  var supportedOptions = ["build", "after_success", "after_failure", "deploy", "platforms", "binary_file"];
+  var neededOptions = ["build", "after_success", "after_failure", "platforms"]
 
   //Check all options are valid
-  for(var i=0;  < parsedYaml.length ; i++)
+  for(var i in parsedYaml)
   {
-    if(!inArray(parsedYaml[i], supportedOptions))
-      throw new SyntaxError("Unsupported option in yaml file: " + parsedYaml[i]);
+    if(!inArray(supportedOptions, i ))
+      throw new SyntaxError("Unsupported option in yaml file: " + i);
   }
 
   //Check needed options
-  //Check all options are valid
-  for(var i=0;  < neededOptions.length ; i++)
+  for(var i in neededOptions)
   {
-    if(!inArray(neededOptions[i], parsedYaml))
+    if(!(neededOptions[i] in parsedYaml))
       throw new SyntaxError("Unspecified mandatory option in yaml file: " + neededOptions[i]);
   }
 }
+
+
 
 
 exports.yaml_parser = yaml_parser;
