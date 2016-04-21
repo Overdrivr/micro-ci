8add87afc409ad44e30a16f9ad5dcf41059bd8ff
Rémi Bèges <remi.beges@gmail.com>
2016-04-21 21:27:24 +0200

    Fix github webhook test
    Return code 204 (no content returned)
    JSON file loading

--- test/unit/webhooks_test.js
+++ test/unit/webhooks_test.js
@@ -1,23 +1,23 @@
 var request = require('supertest');
 var assert = require('chai').assert;
-var app = require('../server/server');
+var app = require('../../server/server');
 var loopback = require('loopback');
 
-var fakepayload = require('fake-github-push-payload.json');
+var fakepayload = require('./fake-github-push-payload.json');
 
 describe('Fake github webhook', function(){
   // See https://developer.github.com/v3/activity/events/types/#events-api-payload-19
   it('should call the endpoint and the content must be properly decoded',
   function(done){
-    request(app)['post']('localhost:3000/api/Repositories/webhook')
-      .set('Content-Type', 'application/json')
-      .send({
-        payload: fakepayload
-      })
-      .expect(200, function(err, res){
+    request(app)
+      .post('/api/Repositories/webhook')
+      .set('Accept', 'application/json')
+      .send(fakepayload)
+      .expect(204, function(err, res){
         if(err) return done(err);
-        // Check a new commit instance was created under the right repo
-        // Check commit instance contains right information
+        //TODO: Check a new commit instance was created under the right repo
+        //TODO: Check commit instance contains right information
+        done();
       })
   });
 });
