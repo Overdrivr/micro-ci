e71495796bddc388ca1d18fd250b99f3f8fed468
Rémi Bèges <remi.beges@gmail.com>
2016-04-27 08:32:10 +0200

    Move conf.js file inside test/e2e/ subfolder

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./test/e2e/test_oauth_auth_github.js'],
+  specs: ['./test_oauth_auth_github.js'],
   /*capabilities: {
     'browserName': 'phantomjs',
     'phantomjs.binary.path': require('phantomjs-prebuilt').path,
