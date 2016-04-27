2b232464ea58ed41e2cc9a478e7a89b4a0eaedb2
Dan <dan.faudemer@gmail.com>
2016-04-27 06:49:59 +0200

    Initial slave manager

--- server/boot/slaves-manager.js
+++ server/boot/slaves-manager.js
@@ -1,14 +1,59 @@
 //var jenkins = require('../../lib/jenkins');
 
+var slaves = [];
+var maxNbOfSlaves = 0;
+var queuedJobs = [];
+
+
+//TODO Migrate all in strongloop by adding a slave mode l
 module.exports = function slavesManager(app) {
 
+  console.log(app.get('nbOfSlaves'));
   console.log("Enabling slaves manager");
-  app.get('/slave/:ip/boot',
+  app.get('/slaveManager/slave/:ip/boot',
     function(req, res){
       console.log(req.params.ip);
       res.status(200).end();
 
-      //Add the slave to jenkins and enable it 
+      //Add the slave to jenkins and enable it TODO
     });
+
+
+  //Ask for a new build
+  app.get('/slaveManager/build/:id/',
+    function(req, res){
+
+      if(slaves.length < maxNbOfSlaves)
+      {
+        //Boot slave TODO
+        var slave = {status : "BOOTING", ip :"", jobId :req.params.id }
+        slaves.push(slave);
+      }
+      else { //Add to the queue list
+        var job = {id:req.params.id};
+        queuedJobs.push(job);
+      }
+
+      res.status(200).end();
+
+      //Add the slave to jenkins and enable it
+    });
+
+
+    app.get('/slaveManager/slave/:ip/end',
+      function(req, res){
+        console.log(req.params.ip);
+        //Find the slave and remove it TODO
+
+        if(queuedJobs.length > 0)
+        {
+
+        }
+
+        res.status(200).end();
+
+        //Add the slave to jenkins and enable it TODO
+      });
+
 
 }
