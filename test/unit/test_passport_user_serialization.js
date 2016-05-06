8b95cac32175bc3aa16f5e85f3f1cc8e44489f56
Rémi Bèges <remi.beges@gmail.com>
2016-05-06 16:24:44 +0200

    Test for deserializeUser

--- test/unit/test_passport_user_serialization.js
+++ test/unit/test_passport_user_serialization.js
@@ -2,7 +2,7 @@ var app = require('../../server/server');
 var assert = require('chai').assert;
 
 describe('user serialization', function(){
-  it('should create a new user the first time', function(done){
+  it('creates a new user the first time', function(done){
     app.serializeUser({
       'provider': 'github',
       'id': 12345
@@ -28,7 +28,7 @@ describe('user serialization', function(){
     });
   });
 
-  it('should not create two users for same provider and provider_id', function(done){
+  it('doesnt create two users for same provider and provider_id', function(done){
     app.serializeUser({
       'provider': 'github',
       'id': 12345
@@ -51,7 +51,23 @@ describe('user serialization', function(){
         assert.equal(instances[0]['provider_id'], 12345)
         done();
       });
+    });
+  });
 
+  it('finds the newly created user', function(done) {
+    app.deserializeUser({
+      'provider': 'github',
+      'id': 12345
+    },
+    function(err, userdata) {
+      if (err) return done(err);
+      assert(userdata, 'userdata is empty.');
+      assert.lengthOf(Object.keys(userdata), 2);
+      assert.property(userdata, 'provider');
+      assert.property(userdata, 'provider_id');
+      assert.deepEqual(userdata.provider, 'github');
+      assert.deepEqual(userdata.provider_id, 12345);
+      done();
     });
   });
 });
