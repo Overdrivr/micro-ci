3801e23b4770c112150a981ce79642772361f0ad
Dan <dan.faudemer@gmail.com>
2016-05-10 08:45:02 +0200

    Correct syntax issue and update todo list

--- server/boot/slaves-manager.js
+++ server/boot/slaves-manager.js
@@ -1,8 +1,10 @@
 var jenkins = require('../../lib/jenkins');
 var slave_api = require('../../lib/localhost_slave_api');
 
-
-
+//TODO put site name (127.0.0.1/ micro-ci.com) in a variable
+//TODO Move as much function as possible in each model
+//TODO manage the err and not throw them
+//TODO replace on method by operation hooks 
 module.exports = function slavesManager(app) {
 
   console.log(app.get('nbOfSlaves'));
@@ -18,7 +20,7 @@ module.exports = function slavesManager(app) {
         Slaves.create({status:"booting"}, function (err)
         {
           if(err)
-            return throw err;
+          throw err;
           slave_api.boot_slave("http://127.0.0.1:3000");
         });
       }
@@ -26,110 +28,105 @@ module.exports = function slavesManager(app) {
   }
 
   app.get('/slaveManager/slave/:ip/boot',
-    function(req, res){//Slave complete is booting operation
-      console.log(req.params.ip);
-      res.status(200).end();
-      Slave.findOne({where:{status:"booting"}}, function(err, slave)
-        if(err)
-          return throw err;
-
-        slave.updateAttributes({status: "Building"}, function(err)
-        {
-          if(err)
-            return throw err;
-        });
-        jenkins.create_node("slave_"+slave.getId(), req.params.ip, function(err)
-        {
-          if(err)
-            return throw err;
-        });
-
-
-      );
-
-
-    });
-
-    //A build is created
-    Builds.on('changed', function (build)
+  function(req, res){//Slave complete is booting operation
+    console.log(req.params.ip);
+    res.status(200).end();
+    Slave.findOne({where:{status:"booting"}}, function(err, slave)
     {
-      if(build.status == "created")
+      if(err)
+      throw err;
+
+      slave.updateAttributes({status: "Building"}, function(err)
       {
-        build.updateAttributes({status: "waiting"}, function(err)
+        if(err)
+        throw err;
+      });
+      jenkins.create_node("slave_"+slave.getId(), req.params.ip, function(err)
+      {
+        if(err)
+        throw err;
+      });
+    });
+  });
+
+  //A build is created
+  app.models.Build.on('changed', function (build)
+  {
+    if(build.status == "created")
+    {
+      build.updateAttributes({status: "waiting"}, function(err)
+      {
+        if(err)
+        throw err;
+
+        //Get yaml content
+        build.job(function(err, job)
+        {
+          //push the build to jenkins
+          jenkins.build(build.getId(), job.yaml, "http://127.0.0.1:3000/build/"+build.getId()+"/complete", function(err)
+          {
+            if(err)
+            throw err;
+
+            check_and_boot_slave();
+          });
+        });
+      });
+    }
+  });
+
+  //A build is complete
+  app.get('/build/:id/complete',
+  function(req, res){
+    console.log(req.params.id);
+    //Update build status to complete:
+    Build.findOne({where:{id:req.params.id}}, function(err, build)
+    {
+      if(err)
+      throw err;
+      //Get build status
+      jenkins.get_build_status(build.getId(), function(err, status)
+      {
+        if(err)
+        throw err;
+        build.updateAttributes({status: status}, function(err)
         {
           if(err)
-          return throw err;
+          throw err;
 
-          //Get yaml content
-          build.job(function(err, job))
-          {
-            //push the build to jenkins
-            jenkins.build(build.getId(), job.yaml, endpoint, function(err) //TODO where is the endpoint?
-            {
-              if(err)
-                return throw err;
-
-              check_and_boot_slave();
-            });
-          });
-
-
-
+          res.status(200).end();
         });
-
-      }
+      });
     });
 
 
-    //A build is complete
-    app.get('/build/:id/complete',
-      function(req, res){
-        console.log(req.params.id);
-        //Update build status to complete:
-        Build.findOne({where:{id:req.params.id}}, function(err, build)
+
+  });
+
+  //A slave is powering down check if we have to start a new one
+  app.get('/slaveManager/slave/:ip/end',
+  function(req, res){
+
+    Slave.findOne({where:{ip:req.params.ip}}, function(err, slave)
+    {
+      if(err)
+      throw err;
+
+      //Remove the slave node from jenkins
+      jenkins.remove_node("slave_" + slave.getId(), function(err) {
+        if(err)
+        throw err;
+
+        //Remove the slave in the db
+        Slave.destroyById(slave.getId(), function(err)
         {
           if(err)
-            return throw err;
-          //Get build status
-          jenkins.get_build_status(build.getId(), function(err, status)
-          {
-            if(err)
-              return throw err;
-              build.updateAttributes({status: status}, function(err)
-              {
-                if(err)
-                  return throw err;
-              });
-          });
-        }
+          throw err;
 
-        res.status(200).end();
-
-      });
-
-      //A slave is powering down check if we have to start a new one
-      app.get('/slaveManager/slave/:ip/end',
-        function(req, res){
-
-          Slave.findOne({where:{ip:req.params.ip}}, function(err, slave)
-          if(err)
-            return throw err;
-
-          //Remove the slave node from jenkins
-          jenkins.remove_node("slave_" + slave.getId(), function(err) {
-            if(err)
-              return throw err;
-
-            //Remove the slave in the db
-            Slave.destroyById(slave.getId(), function(err)
-            {
-              if(err)
-                return throw err;
-
-              check_and_boot_slave();
-            }
-          )
-          });
+          check_and_boot_slave();
+          res.status(200).end();
         });
-
+      });
+    });
+  });
 }
