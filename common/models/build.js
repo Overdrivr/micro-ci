054c5988e3f83799b0c9dffc0389696502441b27
Dan <dan.faudemer@gmail.com>
2016-05-12 07:58:22 +0200

    Move observe in build model

--- common/models/build.js
+++ common/models/build.js
@@ -31,5 +31,39 @@ module.exports = function(Build) {
     }
   );
 
-  
+
+    //A build is created
+    Build.observe('after save', function (ctx, next())
+    {
+      if(ctx.isNewInstance !== undefined)
+      {
+        var build = ctx.instance
+        if(build.status == "created")
+        {
+          build.updateAttributes({status: "waiting"}, function(err)
+          {
+            if(err)
+              throw err;
+
+            //Get yaml content
+            build.job(function(err, job)
+            {
+              //push the build to jenkins
+              jenkins.build(build.getId(), job.yaml, "http://127.0.0.1:3000/build/"+build.getId()+"/complete", function(err)
+              {
+                if(err)
+                  throw err;
+
+                Slave.check_and_boot_slave(function(err) {
+                  if(!err)//We can boot a slave
+                    slave_api.boot_slave("http://127.0.0.1:3000");
+                });
+              });
+            });
+          });
+        }
+      }
+      next();
+    }
+  );
 };
--- server/boot/slaves-manager.js
+++ server/boot/slaves-manager.js
@@ -10,39 +10,5 @@ module.exports = function slavesManager(app) {
   console.log("Enabling slaves manager");
 
 
-  //A build is created
-  Build.observe('after save', function (ctx, next())
-  {
-    if(ctx.isNewInstance !== undefined)
-    {
-      var build = ctx.instance
-      if(build.status == "created")
-      {
-        build.updateAttributes({status: "waiting"}, function(err)
-        {
-          if(err)
-            throw err;
-
-          //Get yaml content
-          build.job(function(err, job)
-          {
-            //push the build to jenkins
-            jenkins.build(build.getId(), job.yaml, "http://127.0.0.1:3000/build/"+build.getId()+"/complete", function(err)
-            {
-              if(err)
-                throw err;
-
-              Slave.check_and_boot_slave(function(err) {
-                if(!err)//We can boot a slave
-                  slave_api.boot_slave("http://127.0.0.1:3000");
-              });
-            });
-          });
-        });
-      }
-    }
-    next();
-  }
-);
 
 }
