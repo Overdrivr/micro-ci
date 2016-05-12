82b9ab9c47944ec835247d6bbe566ac8fc34d97d
Dan <dan.faudemer@gmail.com>
2016-05-12 08:49:50 +0200

    Clean up slaves manager

--- server/boot/slaves-manager.js
+++ server/boot/slaves-manager.js
@@ -1,14 +1,7 @@
-var jenkins = require('../../lib/jenkins'); //TODO this should be available for anyone
-var slave_api = require('../../lib/localhost_slave_api');
+
 
 //TODO put site name (127.0.0.1/ micro-ci.com) in a variable
 //TODO manage the err and not throw them
-//TODO replace on method by operation hooks
 module.exports = function slavesManager(app) {
-
-  console.log(app.get('nbOfSlaves'));
-  console.log("Enabling slaves manager");
-
-
 
 }
