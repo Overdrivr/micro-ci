31e3c3d03165cfef23a8e6c13911eb74a940997c
Rémi Bèges <remi.beges@gmail.com>
2016-05-30 18:45:06 +0200

    Hide all required endpoints

--- common/models/commit.js
+++ common/models/commit.js
@@ -1,3 +1,14 @@
 module.exports = function(Commit) {
-
+  Commit.disableRemoteMethod('create', true);
+  Commit.disableRemoteMethod('upsert', true);
+  Commit.disableRemoteMethod('updateAttributes', false);
+  Commit.disableRemoteMethod('deleteById', true);
+  Commit.disableRemoteMethod('updateAll', true);
+  Commit.disableRemoteMethod('createChangeStream', true);
+  Commit.disableRemoteMethod("count", true);
+  Commit.disableRemoteMethod("findOne", true);
+  Commit.disableRemoteMethod('__create__jobs', false);
+  Commit.disableRemoteMethod('__delete__jobs', false);
+  Commit.disableRemoteMethod('__destroyById__jobs', false);
+  Commit.disableRemoteMethod('__updateById__jobs', false);
 };
