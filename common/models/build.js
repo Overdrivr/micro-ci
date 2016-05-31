6b7f7de534c1a816995d484cc6afe428e7ca3d7c
Dan <dan.faudemer@gmail.com>
2016-05-31 06:59:46 +0200

    Migrate slave manager in build

--- common/models/build.js
+++ common/models/build.js
@@ -2,6 +2,8 @@ var jenkinsConf = require('../../server/jenkins.json');
 var Jenkins = require('../../lib/jenkins');
 var jenkins =  new Jenkins(jenkinsConf.host, jenkinsConf.credential);
 
+//TODO put site name (127.0.0.1/ micro-ci.com) in a variable
+//TODO manage the err and not throw them
 
 module.exports = function(Build) {
   var pendingBuild=0;
@@ -29,6 +31,52 @@ module.exports = function(Build) {
     }
   }
 
+
+  //A build is created
+  Build.observe('after save', function (ctx, next)
+  {
+    if(ctx.isNewInstance !== undefined)
+    {
+      var build = ctx.instance
+      if(build.status == "created")
+      {
+        build.updateAttributes({status: "waiting"}, function(err)
+        {
+          if(err)
+            return next(err);
+
+
+          //Get yaml content
+          build.job(function(err, job)
+          {
+
+            if(err)
+              return next(err);
+            if(job === undefined)
+              return next(new Error("Build should be link to a job"));
+
+
+            //push the build to jenkins
+            jenkins.build(build.getId(), job.yaml, "http://127.0.0.1:3000", function(err)
+            {
+              if(err)
+                return next(err);
+
+              Build.inc_nbPendingBuild();
+              Build.app.models.Slave.check_and_boot_slave(function(err) {
+                if(err) return next(err);
+                return next();
+              });
+            });
+          });
+        });
+      }
+      else
+        return  next();
+    }
+    else
+      return  next();
+  });
 
   Build.complete = function(id, cb)
   {
