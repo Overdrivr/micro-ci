9cc2a8a1c3a1497829dd0db9da8b6be1af047626
Rémi Bèges <remi.beges@gmail.com>
2016-06-03 17:45:43 +0200

    Lint slave.js

--- common/models/slave.js
+++ common/models/slave.js
@@ -1,77 +1,52 @@
+var jenkins   = require('../../server/jenkinsInstance').jenkinsInstance,
+    config    = require('../../server/config'),
+    isIp      = require('is-ip'),
+    slave_api = require('../../lib/localhost_slave_api');
 
-var jenkins = require('../../server/jenkinsInstance').jenkinsInstance;
-
-var config = require('../../server/config');
 var maxNbOfSlaves = config.nbOfSlaves;
-
-var isIp = require('is-ip');
-
-var slave_api = require('../../lib/localhost_slave_api');
 
 module.exports = function(Slave) {
 
-  //This function check if we can powerup a slave and will power up if possible
-  Slave.check_and_boot_slave = function(cb)//Callback is used to inform that you can reate a slave
-  {
-    if(Slave.app.models.Build.get_nbPendingBuild() > 0) // There are pendings build
-    {
-      // if we have not reach the limit of slaves, power up one
-      Slave.count( {}, function(err, cnt){
-        if(err)
-          return cb(err);
-        if(cnt < maxNbOfSlaves)
-        {
-          Slave.create({status:"booting"}, function (err)
-          {
-            if(err)
-              return cb(err);
+  // This function check if we can powerup a slave and will power up if possible
+  // Callback is used to inform that you can reate a slave
+  Slave.check_and_boot_slave = function(cb) {
+    // There are no pendings build
+    if(Slave.app.models.Build.get_nbPendingBuild() <= 0) return cb();
 
-            slave_api.boot_slave("http://"+config.host+":"+config.port, function(err)
-            {
-              if(err)
-                return cb(err);
-              if(err = Slave.app.models.Build.dec_nbPendingBuild())
-                return cb(err)
-              return cb();
+    // if we have not reach the limit of slaves, power up one
+    Slave.count({}, function(err, cnt) {
+      if(err) return cb(err);
+      if(cnt >= maxNbOfSlaves) return cb(new Error('Max number of slave has been reached'));
 
-            });
-          });
-        }
-        else
-          return cb(new Error('Max number of slave has been reached'));
+      Slave.create({status:"booting"}, function (err) {
+        if(err) return cb(err);
+
+        slave_api.boot_slave("http://"+config.host+":"+config.port,
+        function(err) {
+          if(err) return cb(err);
+          if(err = Slave.app.models.Build.dec_nbPendingBuild()) return cb(err);
+          cb();
+        });
       });
-    }
-    else
-      return cb();
+    });
   }
 
 
   Slave.boot = function(ip, cb) {
-    if(isIp(ip))
-    {
-      Slave.findOne({where:{status:"booting"}}, function(err, slave) //At least one slave should be in boot mode
-      {
-        if(err || !slave)
-          return cb(new Error("No slave in booting mode" + slave));
-        slave.updateAttributes({status: "building", IP:ip}, function(err)
-        {
-          if(err)
-            return cb(err)
+    if(!isIp(ip)) return cb(new Error("IP " + ip + " is not a valid IP"));
+    //At least one slave should be in boot mode
+    Slave.findOne({where:{status:"booting"}}, function(err, slave) {
+      if(err || !slave) return cb(new Error("No slave in booting mode" + slave));
 
-          jenkins.create_node(slave.getId(), ip, function(err)
-          {                     
-            if(err)
-              return cb(err)
+      slave.updateAttributes({status: "building", IP:ip}, function(err) {
+        if(err) return cb(err);
 
-            return cb(null, slave.getId());
-          });
+        jenkins.create_node(slave.getId(), ip, function(err) {
+          if(err) return cb(err);
+          cb(null, slave.getId());
         });
-
       });
-    }
-    else {
-      return cb(new Error("IP " + ip + " is not a valid IP"))
-    }
+    });
   }
 
   Slave.remoteMethod(
