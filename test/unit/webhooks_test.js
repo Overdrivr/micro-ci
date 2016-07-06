5237684a911dfa37339b5f6f89467b2b95554321
Rémi Bèges <remi.beges@gmail.com>
2016-07-06 13:21:36 +0200

    Add jenkins nock to webhook_test

--- test/unit/webhooks_test.js
+++ test/unit/webhooks_test.js
@@ -1,18 +1,61 @@
 var request     = require('supertest'),
     clear       = require('clear-require'),
     assert      = require('chai').assert
-    loopback    = require('loopback'),
     fakepayload = require('./fake-github-push-payload.json'),
+    repoId      = null,
     app         = {};
 
-var repoId = null;
+var jenkinsURL = process.env.JENKINS_TEST_URL || 'http://127.0.0.1:8080';
+var serverURL =  'http://0.0.0.0:3000';
 
-describe('Fake github webhook', function(){
+describe('Fake github webhook', function() {
+  var nock = require('nock');
+  var nockJenkins = nock(jenkinsURL);
+  var nockNode = nock(serverURL);
+
+  after(function(done)
+  {
+    if(nock.pendingMocks().length >  0) //Make sure no pending mocks are available. Else it could influence the next test
+      return done(new Error("Pending mocks in nock :"+ nock.pendingMocks()))
+    nock.cleanAll();
+    done();
+  });
 
   // Create a test repository
   before(function(done){
     clear('../../server/server');
     app = require('../../server/server');
+
+    var build_id = 1;
+    var jobName = 'build_' + build_id;
+    var slaveName = 'slave_' + build_id;
+    var slave_id = build_id;
+
+    nockJenkins
+    .head('/job/' + jobName + '/api/json') //Job creation
+    .reply(404)
+    .post('/createItem?name=' + jobName)
+    .reply(200)
+    .post('/job/' + jobName + '/build')
+    .reply(201, '', { location: serverURL + '/queue/item/1/' })
+
+    nockNode.get('/api/Slaves/127.0.0.1/boot')//localhost boot
+    .reply(200);
+
+    build_id = 2;
+    jobName = 'build_' + build_id;
+    slaveName = 'slave_' + build_id;
+    slave_id = build_id;
+    nockJenkins
+    .head('/job/' + jobName + '/api/json') //Job creation
+    .reply(404)
+    .post('/createItem?name=' + jobName)
+    .reply(200)
+    .post('/job/' + jobName + '/build')
+    .reply(201, '', { location: serverURL + '/queue/item/1/' })
+
+    nockNode.get('/api/Slaves/127.0.0.1/boot')//localhost boot
+    .reply(200);
 
     app.models.Repository.create({
       platform: "github",
@@ -83,10 +126,18 @@ describe('Fake github webhook', function(){
       .send(fakepayload)
       .expect(204, function(err, res) {
         if (err) return done(err);
-        done();
+        app.models.Commit.find({
+          'where': {
+            'commithash': fakepayload.after
+          }
+        }, function(err, commits) {
+          if (err) return done(err);
+          assert.strictEqual(commits.length, 1);
+          done();
+        });
       });
   });
-  
+
   //TODO: Test with tampered data (a wrong secret key)
   //TODO: Test POST a commit with same hash but from different platform
 });
